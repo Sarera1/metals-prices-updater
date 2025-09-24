60884a54d87b29244b458d6c1a7de2ea371274a5
Hordii <husdander@gmail.com>
2025-09-24 13:30:32 +0300

    Update update_metals.py

--- update_metals.py
+++ update_metals.py
@@ -1,21 +1,21 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-# --- Ініціалізація Firebase ---
+
 if not firebase_admin._apps:
     cred = credentials.Certificate("serviceAccountKey.json")
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://metals-prices-27b05-default-rtdb.europe-west1.firebasedatabase.app/'
+        'databaseURL': 'https://metals-prices-27b05-default-rtdb.firebaseio.com/'
     })
 
-# --- Дані для оновлення ---
+
 data = {
     "gold": 1925.30,
     "silver": 24.56,
     "platinum": 894.10
 }
 
-# --- Запис у Firebase ---
+
 ref = db.reference("/metals")
 ref.set(data)
 
