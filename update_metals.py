d612edbe3f259411f2216a305c4fad35905e587b
Hordii <husdander@gmail.com>
2025-09-24 14:05:47 +0300

    Update update_metals.py

--- update_metals.py
+++ update_metals.py
@@ -1,26 +1,35 @@
 import firebase_admin
 from firebase_admin import credentials, db
+import os
+import json
 
-# --- Ініціалізація Firebase ---
+# Чтение ключа Firebase из переменной окружения
+firebase_key = os.environ.get('FIREBASE_KEY')
+
+if not firebase_key:
+    raise ValueError("Не найден Firebase ключ в переменных окружения!")
+
+# Преобразуем строку JSON в словарь
+cred_dict = json.loads(firebase_key)
+
+# Инициализация Firebase
+cred = credentials.Certificate(cred_dict)
+
+# Если Firebase не инициализировано, инициализируем
 if not firebase_admin._apps:
-    # Указываем путь к JSON файлу с ключом
-    cred = credentials.Certificate("serviceAccountKey.json")
-    
-    # Инициализация приложения Firebase с указанием URL базы данных
     firebase_admin.initialize_app(cred, {
         'databaseURL': 'https://metals-prices-27b05-default-rtdb.europe-west1.firebasedatabase.app/'
     })
 
-# --- Дані для оновлення ---
+# Данные для обновления
 data = {
     "gold": 1925.30,
     "silver": 24.56,
     "platinum": 894.10
 }
 
-# --- Запис у Firebase ---
-# Ссылка на базу данных, куда будут записываться данные
+# Запись в Firebase
 ref = db.reference("/metals")
-ref.set(data)  # Записываем данные в Firebase
+ref.set(data)
 
 print("✅ Дані оновлено у Firebase")
